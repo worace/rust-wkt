1b170c481d25e860942e58dc9ff253d0c0019229
Horace Williams <horace.d.williams@gmail.com>
2018-07-01 15:58:34 -0700

    Multipoint down

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,12 +1,12 @@
 extern crate geo_types;
 
+use std::convert::From;
 use types::Coord;
 use types::CoordType;
-use std::convert::From;
 // use types::GeometryCollection;
 use types::LineString;
 // use types::MultiLineString;
-// use types::MultiPoint;
+use types::MultiPoint;
 // use types::MultiPolygon;
 use types::Point;
 use types::Polygon;
@@ -21,11 +21,18 @@ pub trait ToGeo<T: geo_types::CoordinateType> {
     fn to_geo(&self) -> Result<geo_types::Geometry<T>, &'static str>;
 }
 
-fn coord_to_g_coord<T: geo_types::CoordinateType + CoordType>(coord: &Coord<T>) -> geo_types::Coordinate<T> {
-    geo_types::Coordinate { x: coord.x, y: coord.y}
+fn coord_to_g_coord<T: geo_types::CoordinateType + CoordType>(
+    coord: &Coord<T>,
+) -> geo_types::Coordinate<T> {
+    geo_types::Coordinate {
+        x: coord.x,
+        y: coord.y,
+    }
 }
 
-fn linestring_to_g_linestring<T: geo_types::CoordinateType + CoordType>(ls: &LineString<T>) -> geo_types::LineString<T> {
+fn linestring_to_g_linestring<T: geo_types::CoordinateType + CoordType>(
+    ls: &LineString<T>,
+) -> geo_types::LineString<T> {
     let mut g_coords: Vec<geo_types::Coordinate<T>> = vec![];
     for c in &ls.0 {
         g_coords.push(coord_to_g_coord(c));
@@ -33,7 +40,9 @@ fn linestring_to_g_linestring<T: geo_types::CoordinateType + CoordType>(ls: &Lin
     g_coords.into_iter().collect()
 }
 
-fn polygon_to_g_polygon<T: geo_types::CoordinateType + CoordType>(poly: &Polygon<T>) -> geo_types::Polygon<T> {
+fn polygon_to_g_polygon<T: geo_types::CoordinateType + CoordType>(
+    poly: &Polygon<T>,
+) -> geo_types::Polygon<T> {
     if poly.0.is_empty() {
         geo_types::Polygon::new(geo_types::LineString(vec![]), vec![])
     } else {
@@ -43,12 +52,22 @@ fn polygon_to_g_polygon<T: geo_types::CoordinateType + CoordType>(poly: &Polygon
         }
 
         let exterior = g_rings.remove(0);
-        geo_types::Polygon{exterior: exterior, interiors: g_rings}
+        geo_types::Polygon {
+            exterior: exterior,
+            interiors: g_rings,
+        }
     }
 }
 
-impl<T: geo_types::CoordinateType + CoordType> ToGeo<T> for Geometry<T>
-{
+fn point_to_g_point<T: geo_types::CoordinateType + CoordType>(point: &Point<T>) -> Option<geo_types::Point<T>> {
+    if let Some(coord) = &point.0 {
+        Some(geo_types::Point(coord_to_g_coord(coord)))
+    } else {
+        None
+    }
+}
+
+impl<T: geo_types::CoordinateType + CoordType> ToGeo<T> for Geometry<T> {
     fn to_geo(self: &Geometry<T>) -> Result<geo_types::Geometry<T>, &'static str> {
         match *self {
             Geometry::Point(Point(Some(ref c))) => {
@@ -56,11 +75,23 @@ impl<T: geo_types::CoordinateType + CoordType> ToGeo<T> for Geometry<T>
                 let g_point = geo_types::Point(g_coord);
                 Ok(geo_types::Geometry::Point(g_point))
             }
-            Geometry::LineString(ref ls) => Ok(geo_types::Geometry::LineString(linestring_to_g_linestring(ls))),
+            Geometry::LineString(ref ls) => Ok(geo_types::Geometry::LineString(
+                linestring_to_g_linestring(ls),
+            )),
             Geometry::Polygon(ref poly) => {
                 Ok(geo_types::Geometry::Polygon(polygon_to_g_polygon(poly)))
             }
-            _ => Err("not implemented")
+            Geometry::MultiPoint(MultiPoint(ref points)) => {
+                let g_points = points
+                    .iter()
+                    .map(|p| point_to_g_point(p))
+                    .filter(|p| p.is_some())
+                    .map(|p| p.unwrap())
+                    .collect();
+                let g_mp = geo_types::MultiPoint(g_points);
+                Ok(geo_types::Geometry::MultiPoint(g_mp))
+            }
+            _ => Err("not implemented"),
         }
     }
 }
@@ -68,11 +99,11 @@ impl<T: geo_types::CoordinateType + CoordType> ToGeo<T> for Geometry<T>
 #[cfg(test)]
 mod tests {
     extern crate geo_types;
-    use types::Coord;
-    use types::Point;
-    use types::LineString;
-    use Geometry;
     use conversion::ToGeo;
+    use types::Coord;
+    use types::LineString;
+    use types::Point;
+    use Geometry;
     use Wkt;
     // use conversion::From;
     // use self::From;
@@ -128,7 +159,9 @@ mod tests {
 
     #[test]
     fn converting_polygon_to_geo() {
-        let wkt: Wkt<f64> = Wkt::from_str("POLYGON ((8 4, 4 0, 0 4, 8 4), (7 3, 4 1, 1 4, 7 3))").ok().unwrap();
+        let wkt: Wkt<f64> = Wkt::from_str("POLYGON ((8 4, 4 0, 0 4, 8 4), (7 3, 4 1, 1 4, 7 3))")
+            .ok()
+            .unwrap();
         assert_eq!(1, wkt.items.len());
         let poly = &wkt.items[0];
         match poly {
@@ -136,10 +169,7 @@ mod tests {
                 let g_poly: geo_types::Polygon<f64> = poly.to_geo().unwrap().as_polygon().unwrap();
 
                 let outer_exp = vec![(8.0, 4.0), (4.0, 0.0), (0.0, 4.0), (8.0, 4.0)];
-                let outer_coords = g_poly.exterior
-                    .into_iter()
-                    .map(|p| p.0)
-                    .map(|c| (c.x, c.y));
+                let outer_coords = g_poly.exterior.into_iter().map(|p| p.0).map(|c| (c.x, c.y));
 
                 for ((x1, y1), (x2, y2)) in outer_coords.zip(outer_exp) {
                     assert_eq!(x1, x2);
@@ -165,5 +195,31 @@ mod tests {
             }
             _ => assert!(false, "should be polygon!"),
         }
+    }
+
+    #[test]
+    fn converting_multi_point_to_geo() {
+        let wkt: Wkt<f64> = Wkt::from_str("MULTIPOINT ((8 4), (4 0))").ok().unwrap();
+        let mp = &wkt.items[0];
+        match mp {
+            Geometry::MultiPoint(_) => {
+                let g_mp: geo_types::MultiPoint<f64> = mp.to_geo().unwrap().as_multipoint().unwrap();
+                let exp = vec![(8.0, 4.0), (4.0, 0.0)];
+                let coords = g_mp.into_iter().map(|p| p.0).map(|c| (c.x, c.y));
+
+                assert_eq!(exp.len(), coords.len());
+
+                for ((x1, y1), (x2, y2)) in coords.zip(exp) {
+                    assert_eq!(x1, x2);
+                    assert_eq!(y1, y2);
+                }
+            }
+            _ => assert!(false, "Should be a MultiPoint"),
+        }
+        // let c: Coord<f64> = Coord::new(12.0, 34.0);
+        // let p = Geometry::Point(Point(Some(c)));
+        // let g_point: geo_types::Point<f64> = p.to_geo().unwrap().as_point().unwrap();
+        // assert_eq!(12.0, g_point.0.x);
+        // assert_eq!(34.0, g_point.0.y);
     }
 }
